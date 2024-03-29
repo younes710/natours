80e094de5d0960f28be04f5d071f9f6eaa0f1b8f
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-29 22:47:42 +0330

    Serving static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 // 1) MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requstTime = new Date().toISOString();
   req.language = 'fa';
