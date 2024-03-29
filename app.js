1830da4d4b351c2efc9585daf305ee85e70b53db
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-29 14:31:16 +0330

    Handling DELETE requests

--- app.js
+++ app.js
@@ -85,6 +85,30 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const tourId = req.params.id * 1;
+
+  if (tourId > tours.length - 1) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const newTours = tours.filter((tour) => tour.id !== tourId);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTours),
+    (err) => {
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running port ${port}...`);
