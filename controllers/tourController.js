480d0573452dfd693c7575fbfd8c6b2479929f4a
Younes Abdi <realyoonesabdi@gmail.com>
2024-04-11 23:31:00 +0330

    Creating a new tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 
 // const checkID = (req, res, next, val) => {
 //   if (val * 1 > tours.length - 1) {
@@ -34,13 +34,25 @@ const getTour = (req, res) => {
   });
 };
 
-const createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+const createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 const updateTour = (req, res) => {
